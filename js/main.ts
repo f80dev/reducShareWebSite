c5b2abed1ee5c286dce7f6db611c91d935884cbf
hhoareau <hhoareau@gmail.Com>
2019-10-28 10:39:02 +0100

    ajout de robots.txt et sitemap.xml

--- js/main.ts
+++ js/main.ts
@@ -13,17 +13,23 @@ function getModels(filter="") {
     fetch("https://reducshare.com/assets/config.json").then(function (r) {
         r.json().then(function (data) {
             config = data;
-            var s = "<table style='display: inline-block;width:70vw;margin-left:3vw;' cellpadding='15px'><tr><th></th><th></th></tr>";
+            var s = "<table style='display: inline-block;width:80vw;margin-left:3vw;' cellpadding='15px'><tr><th></th><th></th></tr>";
             config.modeles.forEach((modele) => {
                 if((filter.length==0 && modele.score>14) || (modele.score>10 && filter.length>0 && (modele.tags.length==0 || modele.tags.indexOf(filter)>-1))){
                     var desc=modele.description;
                     if(desc==null)desc=modele.label;
                     desc=desc+" "+modele.conditions;
+                    if(modele.id==null)modele["id"]="";
+                    var run_url="https://reducshare.com/?command=add_shop,add_promo("+modele.id+")";
+                    var test_button="<br><div style='margin-top:4px;padding:5px;font-size: x-small;pointer-events: none;' class='btn btn-secondary'>Tester</div>";
+
                     if(modele.share_bonus>0)
                         desc=desc+"<br><small>la promotion commence à "+modele.direct_bonus+modele.symbol+", et augmente de 1"+modele.symbol+" supplémentaire chaque fois qu'il la partage "+(1/modele.share_bonus)+" fois</small>";
-                    s = s + "<tr><td style='width:100px;'><img src='"+modele.picture+"' style='width:80px;'></td><td>" + desc + "</td>";
-                    if(modele.id==null)modele["id"]="";
-                    s=s+"<td style='width:30px;'><a href='https://reducshare.com/?command=add_shop,add_promo("+modele.id+")'>Tester</a></td></tr>"
+                    s = s + "<tr><td style='width:100px;text-align: center;margin:0px;'><a href='"+run_url+"'><img src='"+modele.picture+"' style='width:80px;'>"+test_button+"</a></td><td>" + desc + "</td>";
+
+
+
+                    s=s+"</tr>"
                 }
             });
             s = s + "</table>";
